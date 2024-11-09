f01234d563290ed79508079c540c263a0328e915
Jakub Gonera <jakubantoni1@gmail.com>
2024-11-09 18:00:57 +0100

    Introduce the Mailbox class with an example
    Add the mailbox class to log in and send messages with a demo in the main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
-#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "backend/mailbox.h"
 
 int main() {
-	std::cout << "Hello world!\n";
+	std::ifstream configFile("config.json");
+	if (!configFile)
+		std::cerr << "Failed to open config.json\n";
+	nlohmann::json config;
+	configFile >> config;
+
+	std::string senderEmail = config["sender_email"];
+	std::string appPassword = config["app_password"];
+
+	std::ifstream recipientsFile("recipients.txt");
+	if (!recipientsFile)
+		std::cerr << "Failed to open recipients.txt\n";
+
+	std::vector<std::string> recipients;
+	std::string recipient;
+	while (std::getline(recipientsFile, recipient))
+		recipients.push_back(recipient);
+
+	std::string subject = "Test email";
+	std::string body = "This is a test email";
+
+	Mailbox mailbox(senderEmail, appPassword);
+	Message message(recipients, subject, body);
+	mailbox.send(message);
 }
