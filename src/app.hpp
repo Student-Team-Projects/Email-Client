d98ceb9c57a51681be272ecd346579b5183034ea
Hub <bernackihubert03@gmail.com>
2024-11-12 13:09:03 +0100

    Frontend Backend split with Application class as common point

--- src/app.hpp
+++ src/app.hpp
@@ -1,26 +1,20 @@
-#include <string>
-#include "ftxui/component/screen_interactive.hpp"
-#include "ftxui/component/component.hpp"
+#pragma once
+#include <memory>
 
-class application{
+class Application_frontend;
+
+class Application {
 public:
-    void run();
-    application();
-private:
-    enum class state{
+    enum class State {
         EMAIL_DRAFT,
         INBOX,
         SENT_ITEMS
     };
-    void change_state(state);
-
-    state current_state;
-    std::string current_draft;
-    ftxui::Component email_draft;
-    ftxui::Component inbox;
-    ftxui::Component sent_items;
-    ftxui::Component main_component;
-    ftxui::Component control_panel;
-    ftxui::Component layout;
-    ftxui::ScreenInteractive screen;
+    void Run(std::unique_ptr<Application_frontend> front);
+    bool Is_in_state(State state);
+    void Change_state(State new_state);
+    Application();
+private:
+    State current_state;
+    std::unique_ptr<Application_frontend> frontend;
 };
