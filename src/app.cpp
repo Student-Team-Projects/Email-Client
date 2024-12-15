32ec5faa1162a89d2a79e130b7e56f4eb0ecc164
Maciej Brzozowski <maciek.brzozowski149@gmail.com>
2024-12-15 19:37:53 +0100

    fetching works

--- src/app.cpp
+++ src/app.cpp
@@ -10,6 +10,19 @@
 #include <nlohmann/json.hpp>
 
 void Application::Run(std::unique_ptr<Application_frontend> front){
+    std::ifstream configFile("config.json");
+    if (!configFile){
+        std::cerr << "Failed to open config.json"<< std::endl;
+    }
+
+    std::cerr << "managed to open config.json"<< std::endl;
+    nlohmann::json config;
+    configFile >> config;
+    std::string senderEmail = config["sender_email"];
+    std::string appPassword = config["app_password"];
+    Mailbox mailbox(senderEmail, appPassword);
+    mailbox.synchronize();
+    std::vector<Message> emails = mailbox.get_emails();
     frontend = std::move(front);
     frontend->Loop();
 }
