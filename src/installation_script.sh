0442806cbc192f50bec537af368474c01922af44
Hub <bernackihubert03@gmail.com>
2025-01-18 15:23:42 +0100

    installation script

---
+++ src/installation_script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get install libssl-dev
+sudo apt-get install libicu-dev
+sudo apt install libgnutls28-dev libgsasl7-dev
+sudo apt install doxygen
+sudo apt install sendmail
+sudo apt install graphviz
+cmake -G "Unix Makefiles" -DVMIME_SENDMAIL_PATH="/usr/sbin/sendmail" ..
+cd /etc/ssl/
+sudo openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+sudo apt-get install sqlite3 libsqlite3-dev
+sudo apt install libxml2
+sudo apt install libxml2-dev
