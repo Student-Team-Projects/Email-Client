9e71bbc7a010a5fe11a0fb255b0dd307be521dbf
Maciej Mikołajczak <46932793+Surowizator@users.noreply.github.com>
2025-12-03 22:43:58 +0100

    Login dialog

--- src/app.cpp
+++ src/app.cpp
@@ -29,6 +29,7 @@ namespace{
 
 void Application::run(std::unique_ptr<Application_frontend> front){
     frontend = std::move(front);
+    if (!frontend->loginSucceeded) return;
     frontend->run();
 }
 
