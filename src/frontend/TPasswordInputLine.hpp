d0a62218f891b7c241d5cfe70f47da05acccde9a
k-kosiorowski <krzysztof.kosiorowski@student.uj.edu.pl>
2025-12-06 16:25:31 +0100

    Seperate dialogs for logging in and creating accounts

--- src/frontend/TPasswordInputLine.hpp
+++ src/frontend/TPasswordInputLine.hpp
@@ -1,3 +1,6 @@
+#ifndef _TPasswordInpuLine_H_
+#define _TPasswordInpuLine_H_
+
 #define Uses_TInputLine
 #define Uses_TRect
 #include <tvision/tv.h>
@@ -7,5 +10,7 @@ public:
     TPasswordInputLine(TRect bounds, int aMaxLen) 
         : TInputLine(bounds, aMaxLen) {}
 
-    virtual void draw();
+    virtual void draw() override;
 };
+
+#endif
